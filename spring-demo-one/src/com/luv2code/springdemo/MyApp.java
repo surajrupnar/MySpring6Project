2c5d2219cf8ce98bd949113f3b91ef7762973d39
Suraj kundlik Rupnar <suraj@Surajs-Air.lan>
2024-12-08 15:49:17 +0530

    Initial commit

---
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+	public static void main(String[] args) {
+		CricketCoach thecoach = new CricketCoach();
+		System.out.println(thecoach.getDailyWOrkout());
+	}
+}
