6da007a4d0ac0eeea5d31f70ff615b6fb4c94178
Suraj kundlik Rupnar <suraj@MacBookAir.lan>
2025-02-15 13:03:41 +0530

    Setter Injection -injecting values to attributes

--- spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -3,14 +3,34 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach{
 
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String teamName;
 	
 	public CricketCoach() {
 		System.out.println("CricketCoach: Inside no-arg-Constructor");
 	}
 	
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("CricketCoach: Inside the Setter method");
+		System.out.println("CricketCoach: Inside the Setter method - setFortuneService");
 		this.fortuneService = fortuneService;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: Inside the Setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		System.out.println("CricketCoach: Inside the Setter method - setTeamName");
+		this.teamName = teamName;
 	}
 
 	@Override
